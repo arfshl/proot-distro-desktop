efd4fa7ea8c0933860067d97b8adc5f94689faaa
Alif <arfshl@protonmail.com>
2025-09-28 20:03:27 +0700

    manjaro-cinnamon

---
+++ manjaro/cinnamon/bootstrap.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# Install x11 and tur repo
+termux-setup-storage
+apt update
+apt install x11-repo tur-repo
+apt update
+
+# Update installed package but keep configuration
+apt upgrade -y -o Dpkg::Options::="--force-confold"
+
+# Install depedency
+apt install curl wget nano proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y
+
+# Create startup script
+# for CLI session
+echo '#!/bin/sh
+LD_PRELOAD=/system/lib64/libskcodec.so
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+proot-distro login manjaro-cinnamon --user manjaro-cinnamon' >> /data/data/com.termux/files/usr/bin/manjaro-cinnamon
+
+# for X11 session
+cat <<'EOF' > /data/data/com.termux/files/usr/bin/manjaro-cinnamon-x11
+#!/bin/sh
+LD_PRELOAD=/system/lib64/libskcodec.so
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+export XDG_RUNTIME_DIR=${TMPDIR}
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+kill -9 $(pgrep -f "virgl") 2>/dev/null
+proot-distro login manjaro-cinnamon --shared-tmp -- /bin/sh -c 'kill -9 $(pgrep -f "x11") 2>/dev/null'
+virgl_test_server_android &
+termux-x11 :0 >/dev/null &
+sleep 3
+proot-distro login manjaro-cinnamon --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - manjaro-cinnamon -c "DISPLAY=:0 XDG_SESSION_TYPE=x11 dbus-launch --exit-with-session cinnamon-session"'
+EOF
+
+# Make all of them executable
+chmod +x /data/data/com.termux/files/usr/bin/manjaro-cinnamon*
+
+# Install rootfs under aliases
+proot-distro install manjaro --override-alias manjaro-cinnamon
+
+# Setup manjaro-cinnamon
+proot-distro login manjaro-cinnamon -- /bin/sh -c 'pacman -Sy --noconfirm wget'
+
+proot-distro login manjaro-cinnamon -- /bin/sh -c 'wget https://raw.githubusercontent.com/arfshl/proot-distro-desktop/refs/heads/main/arch/cinnamon/install.sh -O install.sh && chmod +x install.sh && ./install.sh && rm install.sh'
+
+echo 'To start command line session: manjaro-cinnamon'
+echo 'To start X11 session: manjaro-cinnamon-x11'
+echo 'To start VNC server: startvnc'
+echo 'To stop VNC server: stopvnc'
+echo 'To restart VNC server: restartvnc'
+echo 'Default user: arch-cinnamon'
+echo 'Default password: 123'    
+echo 'VNC server address: 127.0.0.1:5900'
+echo 'Default VNC password: 1234567890'
