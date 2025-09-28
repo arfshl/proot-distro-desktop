46d92f6771c1e4913cb030898c1698b97157302c
Alif <arfshl@protonmail.com>
2025-09-28 20:10:21 +0700

    Update remove-rootfs.sh

--- manjaro/mate/remove-rootfs.sh
+++ manjaro/mate/remove-rootfs.sh
@@ -4,6 +4,3 @@ proot-distro remove manjaro-mate
 rm /data/data/com.termux/files/usr/bin/manjaro-mate
 rm /data/data/com.termux/files/usr/bin/manjaro-mate-x11
 
-apt remove proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y && apt autoremove -y
-
-apt remove x11-repo tur-repo -y && apt update
