5c893f74fd61515adb321f2b831d4d056104c8ce
Alif <arfshl@protonmail.com>
2025-10-05 10:33:05 +0700

    Update

---
+++ alpine/xfce/install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Update and upgrade system
+apk -U upgrade
+
+# Install Desktop, VNC, and basic utility
+apk add xfce4 xfce4-terminal dbus-x11 pulseaudio nano wget curl sudo adduser xdg-user-dirs xdg-user-dirs-gtk xfce4-whiskermenu-plugin tigervnc xorg pavucontrol -y
+
+# Adding user and password
+sudo adduser --disabled-password --gecos "alpine-xfce" alpine-xfce && echo 'alpine-xfce:123' | chpasswd && echo 'alpine-xfce ALL=(ALL:ALL) ALL' >> /etc/sudoers.d/user
+
+# Setup VNC server
+# Create VNC configuration directory
+mkdir -p /home/alpine-xfce/.vnc
+
+# Create VNC password file (default 1234567890)
+printf "1234567890" | vncpasswd -f > /home/alpine-xfce/.vnc/passwd
+chmod 600 /home/alpine-xfce/.vnc/passwd
+
+# Create VNC startup script
+echo '#!/bin/sh
+xrdb $HOME/.Xresources
+export PULSE_SERVER=127.0.0.1
+export DISPLAY=:0
+dbus-launch --exit-with-session startxfce4' >> /home/alpine-xfce/.vnc/xstartup
+
+# Create script for starting VNC server
+echo "#!/bin/sh
+export USER=alpine-xfce
+export HOME=/home/alpine-xfce
+vncserver -name remote-desktop -localhost no :0
+echo 'VNC server address: 127.0.0.1:5900 Password: 1234567890'" >> /usr/local/bin/startvnc
+
+# Create script for stopping VNC server
+echo '#!/bin/sh
+export USER=alpine-xfce
+export HOME=/home/alpine-xfce
+vncserver -kill :0
+rm -rf /home/alpine-xfce/.vnc/localhost:0.pid
+rm -rf /tmp/.X0-lock
+rm -rf /tmp/.X11-unix/X0' >> /usr/local/bin/stopvnc
+
+# Create script for restarting VNC server
+echo '#!/bin/sh
+stopvnc
+startvnc' >> /usr/local/bin/restartvnc
+
+# Make it executable
+cd /usr/local/bin
+chmod +x startvnc
+chmod +x stopvnc
+chmod +x restartvnc
+cd
+chmod +x /home/alpine-xfce/.vnc/xstartup
