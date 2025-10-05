5775a4779d45241b9800129174b1d5c0c51714b4
Alif <arfshl@protonmail.com>
2025-10-05 10:01:27 +0700

    Update void

---
+++ void/install-chromium.sh
@@ -0,0 +1,5 @@
+sudo xbps-install -S chromium
+sudo sed -i 's|chromium --incognito %U|chromium --incognito --no-sandbox %U|' /usr/share/applications/chromium.desktop
+sudo sed -i 's|chromium %U|chromium --no-sandbox %U|' /usr/share/applications/chromium.desktop
+sudo sed -i 's|chromium --new-window %U|chromium --new-window --no-sandbox %U|' /usr/share/applications/chromium.desktop
+sudo rm /etc/chromium/master_preferences
