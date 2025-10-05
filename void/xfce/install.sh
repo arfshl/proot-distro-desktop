338970a5319fd10c579b58e397df508d803323c2
Alif <arfshl@protonmail.com>
2025-10-05 09:58:40 +0700

    Update xfce

---
+++ void/xfce/install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Update and upgrade system
+xbps-install -Su
+
+# Install Desktop, VNC, and basic utility
+xbps-install -S xfce4 xfce4-terminal dbus-x11 pulseaudio nano wget curl sudo adduser xdg-user-dirs xdg-user-dirs-gtk xfce4-whiskermenu-plugin tigervnc xorg -y && apt clean
+
+# Adding user and password
+sudo useradd -m void-xfce && echo 'void-xfce:123' | chpasswd && echo 'void-xfce ALL=(ALL:ALL) ALL' >> /etc/sudoers.d/user
+
+# Setup VNC server
+# Create VNC configuration directory
+mkdir -p /home/void-xfce/.vnc
+
+# Create VNC password file (default 1234567890)
+printf "1234567890" | vncpasswd -f > /home/void-xfce/.vnc/passwd
+chmod 600 /home/void-xfce/.vnc/passwd
+
+# Create VNC startup script
+echo '#!/bin/sh
+xrdb $HOME/.Xresources
+export PULSE_SERVER=127.0.0.1
+export DISPLAY=:0
+dbus-launch --exit-with-session startxfce4' >> /home/void-xfce/.vnc/xstartup
+
+# Create script for starting VNC server
+echo "#!/bin/sh
+export USER=void-xfce
+export HOME=/home/void-xfce
+vncserver -name remote-desktop -localhost no :0
+echo 'VNC server address: 127.0.0.1:5900 Password: 1234567890'" >> /usr/local/bin/startvnc
+
+# Create script for stopping VNC server
+echo '#!/bin/sh
+export USER=void-xfce
+export HOME=/home/void-xfce
+vncserver -kill :0
+rm -rf /home/void-xfce/.vnc/localhost:0.pid
+rm -rf /tmp/.X0-lock
+rm -rf /tmp/.X11-unix/X0' >> /usr/local/bin/stopvnc
+
+# Create script for restarting VNC server
+echo '#!/bin/sh
+stopvnc
+startvnc' >> /usr/local/bin/restartvnc
+
+# Make it executable
+cd /usr/local/bin
+chmod +x startvnc
+chmod +x stopvnc
+chmod +x restartvnc
+cd
+chmod +x /home/void-xfce/.vnc/xstartup
