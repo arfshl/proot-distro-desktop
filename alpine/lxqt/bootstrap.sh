cedb3f78c563942429abf9599f855ddb2b8c620c
Alif <arfshl@protonmail.com>
2025-10-23 13:02:33 +0700

    Update bootstrap.sh

--- alpine/lxqt/bootstrap.sh
+++ alpine/lxqt/bootstrap.sh
@@ -30,7 +30,7 @@ proot-distro login alpine-lxqt --shared-tmp -- /bin/sh -c 'kill -9 $(pgrep -f "x
 virgl_test_server_android &
 termux-x11 :0 >/dev/null &
 sleep 3
-proot-distro login alpine-lxqt --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - alpine-lxqt -c "DISPLAY=:0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session lxqt"'
+proot-distro login alpine-lxqt --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - alpine-lxqt -c "DISPLAY=:0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session startlxqt"'
 EOF
 
 # Make all of them executable
