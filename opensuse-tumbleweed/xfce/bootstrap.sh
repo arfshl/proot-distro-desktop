a7f264bd5c4b0331f74b8e7055399b9cfa58f355
Alif <arfshl@protonmail.com>
2025-11-06 04:32:12 +0700

    Update bootstrap.sh

--- opensuse-tumbleweed/xfce/bootstrap.sh
+++ opensuse-tumbleweed/xfce/bootstrap.sh
@@ -37,7 +37,7 @@ EOF
 chmod +x /data/data/com.termux/files/usr/bin/opensuse-tumbleweed-xfce*
 
 # Install rootfs under aliases
-PD_OVERRIDE_TARBALL_URL="https://github.com/arfshl/pd-custom-rootfs/releases/download/opensuse-tumbleweed/opensuse-tumbleweed-arm64.tar.xz" PD_OVERRIDE_TARBALL_SHA256="" proot-distro install opensuse --override-alias opensuse-tumbleweed-xfce
+PD_OVERRIDE_TARBALL_URL="https://github.com/arfshl/pd-custom-rootfs/releases/download/opensuse-tumbleweed/opensuse-tumbleweed-aarch64.tar.xz" PD_OVERRIDE_TARBALL_SHA256="" proot-distro install opensuse --override-alias opensuse-tumbleweed-xfce
 
 # Setup opensuse-tumbleweed-xfce
 proot-distro login opensuse-tumbleweed-xfce -- /bin/sh -c 'zypper refresh && zypper -n dup && zypper -n in wget'
